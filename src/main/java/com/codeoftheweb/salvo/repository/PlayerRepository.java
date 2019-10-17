f9c2e3d465e6b0de3b41e3c9dfeee1a25f7a2eee
sbogado <sbogado@secureauth.com>
2019-10-16 21:51:32 -0300

    Agrego algunos endpoints

--- src/main/java/com/codeoftheweb/salvo/repository/PlayerRepository.java
+++ src/main/java/com/codeoftheweb/salvo/repository/PlayerRepository.java
@@ -1,10 +1,10 @@
 package com.codeoftheweb.salvo.repository;
 
-import java.util.List;
-
 import com.codeoftheweb.salvo.model.Player;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+import java.util.List;
 
 @RepositoryRestResource
 public interface PlayerRepository extends JpaRepository<Player, Long> {
